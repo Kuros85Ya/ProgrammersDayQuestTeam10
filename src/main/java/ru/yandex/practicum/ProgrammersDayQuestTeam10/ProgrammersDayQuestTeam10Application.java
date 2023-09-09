3c6b02b0c9e66084693d7fefc4134ec693bdd0f0
avtuman1 <avtuman1@mts.ru>
2023-09-09 14:37:56 +0300

    feat: base

---
+++ src/main/java/ru/yandex/practicum/ProgrammersDayQuestTeam10/ProgrammersDayQuestTeam10Application.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.ProgrammersDayQuestTeam10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammersDayQuestTeam10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammersDayQuestTeam10Application.class, args);
+	}
+
+}
